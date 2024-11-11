2b00bdd8f50838b1228d049a0e50295d98751835
BardosZsolt <71382889+BardosZsolt@users.noreply.github.com>
2024-11-11 14:34:25 +0100

    Add files via upload

--- kezdolap.php
+++ kezdolap.php
@@ -2,26 +2,27 @@
       <div class="col">
         <a href="https://fontmeme.com/trapstar-logo-font/"><img src="https://fontmeme.com/permalink/240213/d4e18533d67d2886f30a4187482b70cf.png" alt="trapstar-logo-font" border="0"></a>        
         <p>A Trapstar egy brit divatmárka, amely streetwear stílusban készült ruházatot kínál, jellemzően hip-hop és urban kultúrával összefonódó dizájnokkal és motívumokkal. A márka elismert a minőségi anyagok és az egyedi tervezésű ruhadarabok iránti elkötelezettségével.</p>
-        <a href="shop.html" target="_blank"><button type="button">Explore</button></a> 
+        <a href="shop.php" target="_blank"><button type="button">Explore</button></a> 
       </div>
       <div class="col">
-      <a href="gallery.html" target="_blank">
+      <a href="./?p=galery" >
           <div class="card card1">
             <h5>Gallery</h5>
             <p></p>
           </div>
         </a>
-        <a href="shop.html" target="_blank">
+        <a href="./?p=shop" >
           <div class="card card2">
             <h5>Shop Online</h5>
             <p></p>
           </div>
         </a>
-        <a href="about.html" target="_blank">
+        <a href="./?p=about" >
           <div class="card card3">
             <h5>About</h5>
             <p></p>
           </div>
+          
         </a>
         <div class="card card4">
           <h5>Community</h5>
