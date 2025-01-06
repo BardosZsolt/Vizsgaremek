56ebf8f7c0d25b5cf980ad610715b8a85fdc6550
dobos-kornel <42321845+dobos-kornel@users.noreply.github.com>
2025-01-06 13:42:11 +0100

    Add files via upload

--- about.php
+++ about.php
@@ -8,19 +8,21 @@
     <link rel="shortcut icon" type="image/jpg" href="images/writing-text.png"/>
   </head>
   <body>
-    <img src="images/trapstar.png" class="bigimg"/>
+    <div class="keret">
+    <a href="https://fontmeme.com/fonts/biko-font/"><img src="https://fontmeme.com/permalink/250106/09df8b763adeecc6a59efa8b33b021ca.png" alt="biko-font" border="0"></a>    </div>
+    
     <div class="flex-container">
       <div>
-        <h2>Trapstar</h2>
+        <h2>Fashionhub</h2>
         <p>
-          A Trapstar egy londoni székhelyű divatmárka, amely a streetwear és a hip-hop kultúra hatását ötvözi stílusos és feltűnő ruházatban. A márka jellegzetes stílusát az urbánus motívumok, mint például a városi tájképek, az utcai művészetek és a hip-hop ikonográfiája határozza meg. A Trapstar ruházat gyakran magában foglalja az egyedi tervezéseket, szimbólumokat és grafikákat, amelyek kifejezik az utcai kultúra és a lázadó szellemiség esszenciáját.</p>
-          <h2>Trapstar a médiában</h2>
-        <p>A márka népszerűsége a közösségi média és a hírességek által történő támogatásának köszönhetően jelentősen megnőtt az elmúlt évek során. Olyan hírességek is viseltek már Trapstar ruhákat, mint Rihanna, Jay-Z és Kanye West, ami tovább növelte a márka elismertségét és vonzerejét a divatvilágban.</p>
+          A Fashionhub egy londoni székhelyű divatmárka, amely a streetwear és a hip-hop kultúra hatását ötvözi stílusos és feltűnő ruházatban. A márka jellegzetes stílusát az urbánus motívumok, mint például a városi tájképek, az utcai művészetek és a hip-hop ikonográfiája határozza meg. A Fashionhub ruházat gyakran magában foglalja az egyedi tervezéseket, szimbólumokat és grafikákat, amelyek kifejezik az utcai kultúra és a lázadó szellemiség esszenciáját.</p>
+          <h2>Fashionhub a médiában</h2>
+        <p>A márka népszerűsége a közösségi média és a hírességek által történő támogatásának köszönhetően jelentősen megnőtt az elmúlt évek során. Olyan hírességek is viseltek már Fashionhub ruhákat, mint Rihanna, Jay-Z és Kanye West, ami tovább növelte a márka elismertségét és vonzerejét a divatvilágban.</p>
           <h2>Kollekciók</h2>
-        <p>A Trapstar kollekciói közé tartoznak pulóverek, pólók, kabátok, sapkák és kiegészítők, amelyeket rendszeresen frissítenek az új stílusok és trendek tükrözésére. A márkára jellemző az innováció és a kreativitás, amelyek egyesítik az utcakultúrát és a divatot egy egyedülálló és megkülönböztető stílusban.</p>
+        <p>A Fashionhub kollekciói közé tartoznak pulóverek, pólók, kabátok, sapkák és kiegészítők, amelyeket rendszeresen frissítenek az új stílusok és trendek tükrözésére. A márkára jellemző az innováció és a kreativitás, amelyek egyesítik az utcakultúrát és a divatot egy egyedülálló és megkülönböztető stílusban.</p>
       </div>
       <div>
-        <img src="images/img3.jpg" style="object-fit: cover" class="bigimg"/>
+        <img src="images/aboutt.png" style="object-fit: cover" class="bigimg"/>
       </div>
     </div>
   </body>
